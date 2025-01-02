682308ef08e6ec89ef551fc994817263c0f039ce
Alihamxa01 <hamza.nadeem.0106@gmail.com>
2025-01-02 17:06:45 +0500

    updated

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -10,12 +10,12 @@ interface BlogCardProps {
 
 export default function BlogCard({ id, title, description, imageUrl }: BlogCardProps) {
   return (
-    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
+    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg transition-transform duration-300  hover:scale-105">
       <Image src={imageUrl} alt={title} width={400} height={200} className="w-full h-48 object-cover" />
       <div className="p-4">
         <h2 className="text-xl font-bold mb-2 text-purple-300">{title}</h2>
         <p className="text-gray-400 mb-4">{description.slice(0, 100)}...</p>
-        <Link href={`/blog/${id}`} className="inline-block bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors duration-300">
+        <Link href={`/blog/${id}`} className=" bg-purple-600 text-white px-5 sm:px-20 py-2 justify-center items-center flex rounded-md hover:bg-purple-700 transition-colors duration-300">
           Read More
         </Link>
       </div>
